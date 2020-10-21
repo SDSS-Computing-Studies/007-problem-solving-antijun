504172ddd9b5b0c67b469bac905d3ebc4c29e01e
antijun <68279777+antijun@users.noreply.github.com>
2020-10-21 13:01:54 -0700

    update 1

---
+++ assignment.py
@@ -0,0 +1,24 @@
+import pyautogui as p
+import time as t
+
+# p.mouseInfo()
+run = True
+
+while run == True:
+    p.click(x=413, y=635)
+    p.click(x=627, y=193)
+    p.click(x=618, y=234)
+
+    t.sleep(1)
+
+    p.click(x=493, y=632)
+    p.click(x=627, y=193)
+    p.click(x=618, y=234)
+
+    t.sleep(1)
+
+    p.click(x=574, y=637)
+    p.click(x=627, y=193)
+    p.click(x=618, y=234)
+
+    t.sleep(1)
